d0f1d035d717db3a0580b6a91e8808e18407c1f0
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-12 15:24:39 +0100

    add icon for todo item, set up rendering of todo item component in todos view

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { defineProps } from "vue";
+import { Icon } from "@iconify/vue";
+
+const props = defineProps({
+  todo: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template lang="">
+  <li>
+    <input type="checkbox" :checked="todo.isCompleted" />
+    <div class="todo">
+      <input type="text" :value="todo.todo" />
+      <span>
+        {{ todo.todo }}
+      </span>
+    </div>
+    <div class="todo-actions">
+        <Icon icon="ph:check-circle" color="#41b080" width="22" />
+    </div>
+  </li>
+</template>
+
+<style lang="scss" scoped></style>
