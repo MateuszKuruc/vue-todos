d16d24a6ba1473402a60d6514f42ba1ce4240fc0
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-12 13:44:59 +0100

    make changes to project structure, add header components, todosview, set routes and styles

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,53 @@
+<script setup></script>
+
+<template>
+  <header>
+    <nav class="container">
+        <div class="branding">
+            <img src="../assets/feniks-logo.png" alt="logo">
+            <h1>Todos app</h1>
+        </div>
+        <ul class="nav-routes">
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+        </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  background-color: #f1f1f1;
+  nav {
+    display: flex;
+    align-items: center;
+    padding: 25px 16px;
+
+    .branding {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      img {
+        max-width: 200px;
+      }
+
+      h1 {
+        font-size: 24px;
+      }
+    }
+
+    .nav-routes {
+      display: flex;
+      flex: 1;
+      justify-content: flex-end;
+      gap: 12px;
+      list-style: none;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+    }
+  }
+}
+</style>
