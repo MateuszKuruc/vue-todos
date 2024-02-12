db36a5e98c628b4f9a21d16c09a8d10d6b7c380e
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-12 18:32:40 +0100

    add local storage handling for todos

--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -6,6 +6,20 @@ import { Icon } from "@iconify/vue";
 import TodoItem from "@/components/TodoItem.vue";
 
 const todoList = ref([]);
+
+const setTodoListLocalStorage = () => {
+  localStorage.setItem("todoList", JSON.stringify(todoList.value));
+};
+
+const fetchTodoList = () => {
+  const savedTodoList = JSON.parse(localStorage.getItem("todoList"));
+  if (savedTodoList) {
+    todoList.value = savedTodoList;
+  }
+};
+
+fetchTodoList();
+
 const createTodo = (todo) => {
   todoList.value.push({
     id: uid(),
@@ -13,22 +27,27 @@ const createTodo = (todo) => {
     isCompleted: null,
     isEditing: null,
   });
+  setTodoListLocalStorage();
 };
 
 const toggleTodoComplete = (todoPos) => {
   todoList.value[todoPos].isCompleted = !todoList.value[todoPos].isCompleted;
+  setTodoListLocalStorage();
 };
 
 const toggleEditTodo = (todoPos) => {
   todoList.value[todoPos].isEditing = !todoList.value[todoPos].isEditing;
+  setTodoListLocalStorage();
 };
 
 const updateTodo = (todoVal, todoPos) => {
   todoList.value[todoPos].todo = todoVal;
+  setTodoListLocalStorage();
 };
 
 const deleteTodo = (todoId) => {
   todoList.value = todoList.value.filter((todo) => todo.id !== todoId);
+  setTodoListLocalStorage();
 };
 </script>
 
